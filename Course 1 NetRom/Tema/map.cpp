ebd93daca822183c34e4ceae343b66b9705fb4ce
Zanfir Bogdan-Andrei <56277980+Ragnarok54@users.noreply.github.com>
2020-01-14 10:58:57 +0200

    Update map.cpp

--- Course 1 NetRom/Tema/map.cpp
+++ Course 1 NetRom/Tema/map.cpp
@@ -1,4 +1,5 @@
 #include <map>
+#include <fstream>
 #include <string>
 
 #include "outputContainer.h"
@@ -47,4 +48,42 @@ void mapOps()
 	newPlaces.insert(places.begin(), places.end());
 	std::cout << "\n New Places after erase and insert: \n";
 	outputMap(newPlaces);
+
+	// Homework
+	std::ifstream f("places.in");
+	std::string currentPlace;
+	std::string city;
+
+	int cities = 0;
+	while (f >> currentPlace) {
+		if ( isdigit(currentPlace.back()) ) {
+			cities = currentPlace.back() - '0';
+			currentPlace = currentPlace.substr(0, currentPlace.length() - 2);
+		}
+		else {
+			cities = 1;
+		}
+		
+		for (int i = 0; i < cities; i++) {
+			f >> city;
+			newPlaces.insert(make_pair(city, currentPlace));
+		}
+	}
+	std::cout << "\n Places after reading from file:\n";
+	outputMap(newPlaces);
+
+	std::cout << "\n\nPlaces in Dolj: ";
+	for (auto it = newPlaces.begin(); it != newPlaces.end(); ++it) {
+		if ((*it).second == "Dolj") {
+			std::cout << (*it).first<<" ";
+		}
+	}
+
+	std::cout << "\n\nPlaces in Oltenia: ";
+	for (auto it = newPlaces.begin(); it != newPlaces.end(); ++it) {
+		if ((*it).second == "Dolj" || (*it).second == "Olt" || (*it).second == "Mehedinti" || (*it).second == "Vrancea" || (*it).second == "Gorj") {
+			std::cout << (*it).first << " ";
+		}
+	}
+	std::cout << "\n";
 }
